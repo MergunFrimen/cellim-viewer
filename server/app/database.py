158da58264d1cd11985c294bbd43a652c2221f50
Dominik Tichy <tichydominik451@gmail.com>
2025-03-11 22:53:57 +0100

    no install

--- server/app/database.py
+++ server/app/database.py
@@ -4,7 +4,6 @@ from sqlalchemy.orm import sessionmaker
 
 from app.shared.config import settings
 
-import os
 
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
