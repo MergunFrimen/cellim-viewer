dd3dda71da12176cd5c33170865a57fb2e268eb4
Dominik Tichy <tichydominik451@gmail.com>
2025-04-13 15:19:15 +0200

    wip

--- backend/app/api/v1/contracts/requests/view.py
+++ backend/app/api/v1/contracts/requests/view.py
@@ -1,17 +1,17 @@
-from uuid import UUID
-
-from fastapi import Form
+from fastapi import File, Form, UploadFile
 from pydantic import BaseModel
 
 
 class ViewRequest(BaseModel):
     name: str = Form(max_length=255, examples=["View Name"])
-    description: str | None = Form(default=None, examples=["Markdown description."])
+    description: str | None = Form(default=None, max_length=255, examples=["View Description"])
 
 
-# TODO: figure out how to put File and Form stuff into pydantic model
 class ViewCreateRequest(ViewRequest):
-    pass
+    thumbnail_image: UploadFile | None = File(default=None, description="Thumbnail image for view")
+    snapshot_json: UploadFile | None = File(
+        default=None, description="Mol* state file (.molj file)"
+    )
 
 
 class ViewUpdateRequest(ViewRequest):
--- backend/app/api/v1/endpoints/views.py
+++ backend/app/api/v1/endpoints/views.py
@@ -1,11 +1,11 @@
 from typing import Annotated
 from uuid import UUID, uuid4
 
-from fastapi import APIRouter, Body, Depends, File, Form, HTTPException, Path, UploadFile
+from fastapi import APIRouter, Body, Depends, File, HTTPException, Path
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
-from app.api.v1.contracts.requests.view import ViewUpdateRequest
+from app.api.v1.contracts.requests.view import ViewCreateRequest, ViewUpdateRequest
 from app.api.v1.contracts.responses.views import ViewResponse
 from app.api.v1.tags import Tags
 from app.database.models.entry import Entry
@@ -19,45 +19,44 @@ router = APIRouter(prefix="/entry/{entry_id}/views", tags=[Tags.views])
 @router.post("", status_code=status.HTTP_201_CREATED, response_model=ViewResponse)
 async def create_view_with_image(
     entry_id: Annotated[UUID, Path(title="Entry ID")],
-    name: Annotated[str, Form(max_length=255, examples=["View Name"])],
-    description: Annotated[str, Form(max_length=255, examples=["View Description"])],
+    request: Annotated[ViewCreateRequest, File()],
     session: Annotated[AsyncSession, Depends(get_async_session)],
-    thumbnail_image: Annotated[UploadFile | None, File()] = None,
-    snapshot_json: Annotated[UploadFile | None, File()] = None,
 ) -> ViewResponse:
     result = await session.get(Entry, entry_id)
     if not result:
         raise HTTPException(status_code=404, detail="Entry not found")
+
+    print(request)
 
     view_id = uuid4()
     thumbnail_url: str | None = None
     snapshot_url: str | None = None
 
     # save snapshot
-    if snapshot_json:
+    if request.snapshot_json:
         try:
             snapshot_url = await file_storage.save_view_snapshot(
                 view_id,
-                snapshot_json.file,
+                request.snapshot_json.file,
             )
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Error saving JSON snapshot: {str(e)}")
 
     # save image
-    if thumbnail_image:
+    if request.thumbnail_image:
         try:
             thumbnail_url = await file_storage.save_view_image(
                 view_id,
-                thumbnail_image.file,
-                thumbnail_image.filename,
+                request.thumbnail_image.file,
+                request.thumbnail_image.filename,
             )
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Error saving image: {str(e)}")
 
     new_view = View(
         id=view_id,
-        name=name,
-        description=description,
+        name=request.name,
+        description=request.description,
         snapshot_url=snapshot_url,
         thumbnail_url=thumbnail_url,
     )
