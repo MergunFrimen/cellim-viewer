98265f33961494816beaea51bae5202efcc878b8
Dominik Tichy <tichydominik451@gmail.com>
2025-04-22 14:35:55 +0200

    wip

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import auth, entries, views
+from app.api.v1.endpoints import auth, entries, test, views
 from app.core.settings import get_settings
 
 v1_api_router = APIRouter(prefix=get_settings().API_V1_PREFIX)
 
+v1_api_router.include_router(test.router)
 v1_api_router.include_router(auth.router)
 v1_api_router.include_router(entries.router)
 v1_api_router.include_router(views.router)
---
+++ backend/app/api/v1/endpoints/test.py
@@ -0,0 +1,35 @@
+from functools import lru_cache
+
+import httpx
+from fastapi import APIRouter, BackgroundTasks
+
+router = APIRouter(prefix="/test", tags=["tags"])
+
+
+class AirportService:
+    async def get_airport_position(
+        self,
+        iata_code: str,
+    ):
+        airport_response = await httpx.AsyncClient().get(
+            f"https://airport-data.com/api/ap_info.json?iata={iata_code}"
+        )
+        airport_data = airport_response.json()
+        lat = airport_data["latitude"]
+        lon = airport_data["longitude"]
+
+        print("DONE", lat, lon)
+
+
+@lru_cache
+def get_airport_service():
+    return AirportService()
+
+
+@router.get("/background-task")
+async def background_task(background_tasks: BackgroundTasks):
+    background_tasks.add_task(get_airport_service().get_airport_position, "ORD")
+    background_tasks.add_task(get_airport_service().get_airport_position, "ORD")
+    background_tasks.add_task(get_airport_service().get_airport_position, "ORD")
+    background_tasks.add_task(get_airport_service().get_airport_position, "ORD")
+    return 123
