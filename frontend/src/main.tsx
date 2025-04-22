2c55061df146ad7a36f714d87ab13300114154b8
Dominik Tichy <tichydominik451@gmail.com>
2025-04-22 23:24:36 +0200

    wip

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import { client } from "@/client/client.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
@@ -7,6 +8,10 @@ import { MolstarProvider } from "./context/MolstarContext.tsx";
 import { ThemeProvider } from "./context/ThemeProvider.tsx";
 
 import "./index.css";
+
+client.setConfig({
+  auth: () => "TOKEN",
+});
 
 const queryClient = new QueryClient();
 
