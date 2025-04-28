d0cf945349a791844f554fef947b9c218506fa81
Dominik Tichy <tichydominik451@gmail.com>
2025-04-29 00:09:46 +0200

    wip

--- frontend/src/components/entries/EntryPreview.tsx
+++ frontend/src/components/entries/EntryPreview.tsx
@@ -8,16 +8,18 @@ export function EntryPreview({ entry }: { entry: EntryDetailsResponse }) {
   return (
     <Card
       key={entry.id}
-      className="overflow-hidden hover:shadow-lg transition-shadow min-h-96"
+      className="overflow-hidden hover:shadow-lg transition-shadow min-h-72"
     >
       <div className="aspect-video bg-secondary flex items-center justify-center">
         <ImageIcon size={64} className="text-muted-foreground" />
       </div>
-      <CardContent className="grow">
-        <h3 className="font-bold text-lg">{entry.name}</h3>
-        <p className="text-muted-foreground text-sm">{entry.description}</p>
+      <CardContent className="flex flex-col flex-grow">
+        <h3 className="font-bold text-lg mb-2">{entry.name}</h3>
+        <p className="text-muted-foreground text-sm line-clamp-4 overflow-hidden text-ellipsis">
+          {entry.description}
+        </p>
       </CardContent>
-      <CardFooter className="justify-between">
+      <CardFooter className="justify-end">
         <Button variant="secondary" size="sm" asChild>
           <Link to={`/entries/${entry.id}`}>View Details</Link>
         </Button>
