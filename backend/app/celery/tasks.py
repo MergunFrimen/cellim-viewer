5f333dd1896d2d07c5dac4a9616c3f350716bcfd
Dominik Tichy <tichydominik451@gmail.com>
2025-05-05 20:11:14 +0200

    wip

--- backend/app/celery/tasks.py
+++ backend/app/celery/tasks.py
@@ -1,19 +1,12 @@
-import time
+import asyncio
 from typing import Any
 
-from app.api.v1.endpoints.test_endpoint import get_airport_service
 from app.celery.celery_app import app
 
 
-@app.task()
-async def process_large_dataset(seconds: int) -> dict[str, Any]:
-    time.sleep(seconds)
-
-    # await get_airport_service().get_airport_position, "ORD"
-    # await get_airport_service().get_airport_position, "ORD"
-    # await get_airport_service().get_airport_position, "ORD"
-    # await get_airport_service().get_airport_position, "ORD"
-    # await get_airport_service().get_airport_position, "ORD"
+@app.task(bind=True)
+async def process_large_dataset(self, seconds: int) -> dict[str, Any]:
+    await asyncio.sleep(seconds)
 
     result = {
         "status": "heavy task finished",
